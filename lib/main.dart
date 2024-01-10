e335ed21858abc304c21d19ceb29e68ed1e24b33
Reza <akdev40032@gmail.com>
2024-01-10 12:43:16 +0330

    add Center to Container

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _HomePageState extends State<HomePage> {
             children: [],
           ),
           Container(
-            child: ,
+            child: Center(),
           )
         ],
       ),
